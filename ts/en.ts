b1721dc2d3ae116b49754d00ebd74736c829aff1
Mr.MG <mg17.mohammad@gmail.com>
2020-06-04 22:39:35 +0430

    بهبود ترجمه ها

--- ts/en.ts
+++ ts/en.ts
@@ -2,14 +2,22 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US" sourcelanguage="fa_IR">
 <context>
+    <name>AppHeader</name>
+    <message>
+        <location filename="../qml/AppHeader.qml" line="21"/>
+        <source>مموریتو</source>
+        <translation>Memorito</translation>
+    </message>
+</context>
+<context>
     <name>DrawerBody</name>
     <message>
-        <location filename="../qml/DrawerBody.qml" line="79"/>
+        <location filename="../qml/DrawerBody.qml" line="78"/>
         <source>تنظیمات</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../qml/DrawerBody.qml" line="83"/>
+        <location filename="../qml/DrawerBody.qml" line="82"/>
         <source>پروژه ها</source>
         <translation>Projects</translation>
     </message>
@@ -17,12 +25,12 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="8"/>
+        <location filename="../qml/main.qml" line="10"/>
         <source>مموریتو</source>
         <translation>Memorito</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="148"/>
+        <location filename="../qml/main.qml" line="205"/>
         <source>تغییر زبان</source>
         <translation>Change Language</translation>
     </message>
