416e0a0653922f0710ca24b13cc3b9520121b372
Mr.MG <mg17.mohammad@gmail.com>
2020-12-21 00:47:28 +0330

    جلوگیری از overwrite کردن فایل وقتی وجود دارد

--- cpp/tools.cpp
+++ cpp/tools.cpp
@@ -7,11 +7,27 @@ Tools::Tools(QObject *parent) : QObject(parent)
 
 QString Tools::saveBase64asFile(QString fileName, QString fileExtensions, QString base64data)
 {
-    QSaveFile file(QDir::homePath()+"/"+fileName+"."+fileExtensions);
+    QString saveDir(QDir::homePath()+"/Memorito Data");
+    QString saveFile(saveDir+"/"+fileName+"."+fileExtensions);
+    QDir folder(saveDir);
+
+    if(!folder.exists())
+    {
+        folder.mkdir(saveDir);
+    }
+
+    QFile checkExist(saveFile);
+    if(checkExist.exists())
+    {
+        return saveFile+"Mohammad";
+    }
+
+    QSaveFile file(saveFile);
     file.open(QIODevice::WriteOnly);
     file.write(QByteArray::fromBase64(base64data.toLatin1()));
     file.commit();
-    return (QDir::homePath()+"/"+fileName+"."+fileExtensions);
+
+    return saveFile;
 }
 
 QString Tools::encodeToBase64(QString filePath)
