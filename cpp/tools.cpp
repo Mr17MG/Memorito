b420927770ca1f7e4a7b0187f8dd71c22cd0d15f
Mr.MG <mg17.mohammad@gmail.com>
2021-01-02 00:34:40 +0330

    اضافه شدن سه تابع برای بررسی اطلاعات فایل،بررسی وجود داشتن فایل دانلود شده، و گرفتن دایرکتور ذخیره فایل ها

--- cpp/tools.cpp
+++ cpp/tools.cpp
@@ -2,24 +2,24 @@
 
 Tools::Tools(QObject *parent) : QObject(parent)
 {
+    saveDir = (QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation)+QDir::separator()+"Memorito Data"+QDir::separator());
 
 }
 
 QString Tools::saveBase64asFile(QString fileName, QString fileExtensions, QString base64data)
 {
-    QString saveDir(QDir::homePath()+"/Memorito Data");
-    QString saveFile(saveDir+"/"+fileName+"."+fileExtensions);
-    QDir folder(saveDir);
+    QString saveFile(this->saveDir+fileName+"."+fileExtensions);
+    QDir folder(this->saveDir);
 
     if(!folder.exists())
     {
-        folder.mkdir(saveDir);
+        folder.mkdir(this->saveDir);
     }
 
     QFile checkExist(saveFile);
     if(checkExist.exists())
     {
-        return saveFile+"Mohammad";
+        return saveFile;
     }
 
     QSaveFile file(saveFile);
@@ -44,4 +44,33 @@ QString Tools::encodeToBase64(QString filePath)
     }
     else return "";
 
+}
+
+QVariantMap Tools::getFileInfo(QString filePath)
+{
+    filePath.remove("file://");
+
+    QFileInfo fileInfo(QUrl(filePath).toString());
+
+    QVariantMap map;
+
+    if(fileInfo.exists())
+    {
+        map.insert("file_size", fileInfo.size());
+        map.insert("file_extension",fileInfo.suffix());
+        map.insert("file_source",fileInfo.filePath());
+        map.insert("file_name",fileInfo.baseName());
+    }
+    return map;
+}
+
+bool Tools::checkFileExist(QString fileName, QString fileExtension)
+{
+    QFileInfo fileInfo(QUrl(this->saveDir+QDir::separator()+fileName+"."+fileExtension).toString());
+    return fileInfo.exists();
+}
+
+QString Tools::getSaveDirectory()
+{
+    return this->saveDir;
 }
--- cpp/tools.h
+++ cpp/tools.h
@@ -6,15 +6,21 @@
 #include <QFile>
 #include <QSaveFile>
 #include <QDir>
+#include <QVariantMap>
+#include <QUrl>
+#include <QStandardPaths>
 
 class Tools : public QObject
 {
     Q_OBJECT
+    QString saveDir;
 public:
     explicit Tools(QObject *parent = nullptr);
     Q_INVOKABLE QString saveBase64asFile(QString fileName, QString fileExtensions, QString base64data);
     Q_INVOKABLE QString encodeToBase64(QString filePath);
-
+    Q_INVOKABLE QVariantMap getFileInfo(QString filePath);
+    Q_INVOKABLE bool checkFileExist(QString fileName,QString fileExtension);
+    Q_INVOKABLE QString getSaveDirectory();
 signals:
 
 };
